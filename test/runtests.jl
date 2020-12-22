a594b3afce270acdb7577398378b2b365b315cfd
Rob J Goedman <goedman@mac.com>
2020-12-22 09:32:46 -0700

    Rel 2.1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,13 @@
 using StanOptimize, Test
 
-@testset "Bernoulli optimize" begin
-  include(joinpath(@__DIR__, "../examples/Bernoulli/bernoulli.jl"))
-  @test optim1["theta"][end] ≈ 0.3 atol=0.1
-  @test optim2["theta"][end] ≈ 0.3 atol=0.1
-end
+if haskey(ENV, "JULIA_CMDSTAN_HOME")
 
+    @testset "Bernoulli optimize" begin
+      include(joinpath(@__DIR__, "../examples/Bernoulli/bernoulli.jl"))
+      @test optim1["theta"][end] ≈ 0.3 atol=0.1
+      @test optim2["theta"][end] ≈ 0.3 atol=0.1
+    end
+
+else
+  println("\nJULIA_CMDSTAN_HOME not set. Skipping tests")
+end
