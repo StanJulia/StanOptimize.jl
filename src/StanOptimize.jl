2d6158dea444d43cf4101b9e859cb891e420aa17
Rob J Goedman <goedman@icloud.com>
2021-11-06 10:13:31 -0600

    Rel 3.1.0

--- src/StanOptimize.jl
+++ src/StanOptimize.jl
@@ -16,6 +16,10 @@ using StanBase
 
 import StanBase: update_model_file, par, handle_keywords!
 import StanBase: executable_path, ensure_executable, stan_compile
+import StanBase: update_R_files
+import StanBase: data_file_path, init_file_path, sample_file_path
+import StanBase: generated_quantities_file_path, log_file_path
+import StanBase: diagnostic_file_path, setup_diagnostics
 
 include("stanmodel/OptimizeModel.jl")
 
